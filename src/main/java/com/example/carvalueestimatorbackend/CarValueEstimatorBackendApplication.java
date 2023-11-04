954ca21d9f7fe33873cf648a3323e43898fd62a0
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2023-11-04 09:22:16 +0300

    Initial commit

---
+++ src/main/java/com/example/carvalueestimatorbackend/CarValueEstimatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.carvalueestimatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarValueEstimatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarValueEstimatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carvalueestimatorbackend/CarValueEstimatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carvalueestimatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarValueEstimatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
